a4509cd366dcee73afdeadda966bb301a1477a35
李依若 <401213245@qq.com>
2020-05-26 21:34:30 +0800

    work-fair模式 公平分发，理应能者多劳 完成工作。 （出现了问题，工作 有遗漏，部分数据应当输出 未输出）

---
+++ src/main/java/com/liyiruo/rabbitmq/workfair/Receive2.java
@@ -0,0 +1,38 @@
+package com.liyiruo.rabbitmq.workfair;
+
+import com.liyiruo.rabbitmq.util.ConnectUtil;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Receive2 {
+    private static final String QUEUE_NAME = "test_work_fair_queue";
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        //获取连接
+        Connection connection = ConnectUtil.getConnection();
+        //获取channel
+        Channel channel = connection.createChannel();
+        //声明队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        channel.basicQos(1);//保证依次只接收一个消息
+        DefaultConsumer defaultConsumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String msg = new String(body, "UTF-8");
+                System.out.println("消费者2=>" + msg);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    //手动回执消息
+                    channel.basicAck(envelope.getDeliveryTag(),false);
+                }
+            }
+        };
+        boolean autoAck = false;// work fair 关闭自动应答
+        channel.basicConsume(QUEUE_NAME, autoAck, defaultConsumer);
+    }
+}
