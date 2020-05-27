bf8ed817af810a3e6c1c2a7415dbaad950714494
liyiruo <401213245@qq.com>
2020-05-28 00:09:27 +0800

    路由模式，也是绑定交换机和队列的时候报错！！

---
+++ src/main/java/com/liyiruo/rabbitmq/routing/Revice1.java
@@ -0,0 +1,39 @@
+package com.liyiruo.rabbitmq.routing;
+
+import com.liyiruo.rabbitmq.util.ConnectUtil;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Revice1 {
+    private static final String EXCHANGE_NAME = "test_exchange_direct";
+    private static final String QUEUE_NAME = "test_queue_direct1";
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = ConnectUtil.getConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(EXCHANGE_NAME, false, false, false, null);
+        channel.basicQos(1);
+        channel.exchangeBind(QUEUE_NAME, EXCHANGE_NAME, "error");
+
+        DefaultConsumer defaultConsumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String msg = new String(body, "utf-8");
+                System.out.println(msg);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    channel.basicAck(envelope.getDeliveryTag(),false);
+                }
+            }
+        };
+
+        boolean autoAck=false;
+        channel.basicConsume(QUEUE_NAME, autoAck, defaultConsumer);
+
+
+    }
+}
