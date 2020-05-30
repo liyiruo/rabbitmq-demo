4682e3b307736c15365496d51d37d6c68fcda7d6
liyiruo <401213245@qq.com>
2020-05-31 00:29:37 +0800

    confirm  消费者收不到消息。

---
+++ src/main/java/com/liyiruo/rabbitmq/confirm/Send1.java
@@ -0,0 +1,30 @@
+package com.liyiruo.rabbitmq.confirm;
+
+import com.liyiruo.rabbitmq.util.ConnectUtil;
+import com.rabbitmq.client.*;
+
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Send1 {
+    private static final String QUEUE_NAME = "test_queue_configrm1";
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        Connection connection = ConnectUtil.getConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        //生产者调用 confirmSelect  将channel 设置为confirm 模式
+        channel.confirmSelect();
+        String msg = "hello configrm1";
+        channel.basicPublish("","QUEUE_NAME",null,msg.getBytes());
+
+        if (!channel.waitForConfirms()) {
+            System.out.println("message send is failed");
+        } else {
+            System.out.println("message send is ok");
+        }
+        channel.close();
+        connection.close();
+    }
+}
